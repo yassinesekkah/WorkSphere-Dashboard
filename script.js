ad68332f7372b005ab1e79d06dbbf3873743f2dc
Yassine Sekkah <yassinesekkah11@gmail.com>
2025-11-18 12:14:49 +0100

    feat: save data form in a globale array

--- script.js
+++ script.js
@@ -5,23 +5,35 @@ const openForm = document.querySelector(".ajouterButton");
 const cancelButtonForm = document.getElementById("cancelButtonForm");
 const ajouterButtonForm = document.getElementById("ajouterButtonForm");
 
-
 const nonEffectueContainer = document.getElementById("nonEffeContain");
 let nonEffectueArray = [];
+let experienceArray = [];
+
 ///=====>>>> for open the big form <<<<=====\\\
 openForm.addEventListener("click", function () {
   bigModal.classList.remove("hideModel")
 })
 ///=====>>>> for close the big form <<<<=====\\\
 cancelButtonForm.addEventListener("click", function () {
-  bigModal.classList.add("hideModel")
+  //====> clear the inputs
+  document.getElementById("nameInput").value = "";
+  document.getElementById("roleSelect").value = "";
+  document.getElementById("photoInput").value = "";
+  document.getElementById("emailInput").value = "";
+  document.getElementById("phoneInput").value = "";
+  document.getElementById("expList").innerHTML = "";
+
+  document.getElementById("expJob").value = "";
+  document.getElementById("expCompany").value = "";
+  document.getElementById("expYears").value = "";
+  bigModal.classList.add("hideModel");
 })
 ///=====>>>> for toggle the experience form <<<<=====\\\
 experienceOpenButton.addEventListener("click", function () {
   experienceForm.classList.toggle("showForm");
 });
 
-///=====>>>> for toggle the experience form <<<<=====\\\
+///=====>>>> for add a new experience <<<<=====\\\
 function confirmExperience() {
   const job = document.getElementById("expJob").value;
   const company = document.getElementById("expCompany").value;
@@ -31,11 +43,21 @@ function confirmExperience() {
     alert("Veuillez remplir tous les champs de l'expérience.");
     return;
   }
+  const expId = Date.now();
+  //===> experience object
+  experienceArray.push({
+    expId : expId,
+    jobName : job,
+    companyName : company,
+    periode : years
+  })
+  console.log("hada experienceArray", experienceArray);
 
   const expList = document.getElementById("expList");
 
   const wrapper = document.createElement("div");
   wrapper.className = "experienceItem";
+  wrapper.setAttribute("data-id", expId);
 
   wrapper.innerHTML = `
     <strong>${job}</strong> — ${company} (${years})
@@ -51,7 +73,14 @@ function confirmExperience() {
 }
 
 function removeExperience(btn) {
-  btn.parentElement.remove();
+  let item = btn.parentElement;
+  let id = Number(item.getAttribute("data-id"));
+  //====> filter expArray with item id
+  experienceArray =  experienceArray.filter(exp => exp.expId !== id);
+  //======> remove the item in UI
+  item.remove();
+
+  console.log("hada l array mn ba3d lremove", experienceArray);
 }
 
 ///=====>>>> for ajouter non effectue employes <<<<=====\\\
@@ -61,7 +90,7 @@ ajouterButtonForm.addEventListener("click", function () {
   const roleSelect = document.getElementById("roleSelect").value.trim();
   const photoInput = document.getElementById("photoInput").value.trim();
   const emailInput = document.getElementById("emailInput").value.trim();
-  const phneInput = document.getElementById("phoneInput").value.trim();
+  const phoneInput = document.getElementById("phoneInput").value.trim();
 
   let divContainCard = document.createElement("div");
   divContainCard.className = "divContainCard";
@@ -85,6 +114,35 @@ ajouterButtonForm.addEventListener("click", function () {
   nameAndRoleContainer.appendChild(cardRole);
 
   nonEffectueContainer.appendChild(divContainCard);
+  //===> cart object
+  let cardObject = {
+    name: nameInput,
+    role: roleSelect,
+    photo: photoInput,
+    email: emailInput,
+    phone: phoneInput,
+    experience :experienceArray
+  }
+  console.log("thaat", cardObject);
+  //====> push to the no effectue array
+  nonEffectueArray.push(cardObject);
+  console.log("this is the array ", nonEffectueArray);
 
+  //====> clear the inputs
+  document.getElementById("nameInput").value = "";
+  document.getElementById("roleSelect").value = "";
+  document.getElementById("photoInput").value = "";
+  document.getElementById("emailInput").value = "";
+  document.getElementById("phoneInput").value = "";
+  document.getElementById("expList").innerHTML = "";
+
+  document.getElementById("expJob").value = "";
+  document.getElementById("expCompany").value = "";
+  document.getElementById("expYears").value = "";
+
+  //====> close the big form
+  bigModal.classList.add("hideModel");
+  
+  
 })
 
